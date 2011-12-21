633271cb8b948298e49b748aab0c335786ce6c25
Matteo Riondato <matteo_riondato@brown.edu>
2011-12-21 14:44:06 +0100

    Modify the driver class to execute the aggregate phase after we computed the local FI's from the sample. The number of input arguments increased from 2 to 3.

--- src/artool/ParallelFrequentItemsetMiner.java
+++ src/artool/ParallelFrequentItemsetMiner.java
@@ -3,10 +3,12 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.lib.IdentityMapper;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.KeyValueTextInputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -15,9 +17,9 @@ public class ParallelFrequentItemsetMiner extends Configured implements Tool
 		
 	public static void main(String args[]) throws Exception
 	{
-		if (args.length != 2)
+		if (args.length != 3)
 		{
-			System.out.println("usage: <path to input database> <path to output files>");
+			System.out.println("usage: <path to input database> <path to output local FIs> <path to output global FIs>");
 			System.exit(1); 
 		}
 
@@ -60,6 +62,35 @@ public class ParallelFrequentItemsetMiner extends Configured implements Tool
 			
 		System.out.println("total job runtime (seconds): " + job_runtime); 
 
+		JobConf confAggr = new JobConf(getConf());
+
+		confAggr.setBoolean("mapred.reduce.tasks.speculative.execution", false); 
+		confAggr.setInt("mapred.task.timeout", 60000000); 
+
+		confAggr.setJarByClass(ParallelFrequentItemsetMiner.class);
+			
+		confAggr.setMapOutputKeyClass(Text.class); 
+		confAggr.setMapOutputValueClass(Text.class); 
+			
+		confAggr.setOutputKeyClass(Text.class); 
+		confAggr.setOutputValueClass(DoubleWritable.class); 
+			
+		confAggr.setMapperClass(IdentityMapper.class);
+		confAggr.setReducerClass(AggregateReducer.class);
+			
+		confAggr.setInputFormat(KeyValueTextInputFormat.class);
+
+		KeyValueTextInputFormat.addInputPath(confAggr, new Path(args[1]));
+		FileOutputFormat.setOutputPath(confAggr, new Path(args[2]));
+
+		job_start_time = System.currentTimeMillis(); 
+		JobClient.runJob(confAggr);
+		job_end_time = System.currentTimeMillis(); 
+			
+		job_runtime = (job_end_time-job_start_time) / 1000; 
+			
+		System.out.println("total aggregation job runtime (seconds): " +
+				job_runtime); 
 
 		return 0;
 	}
