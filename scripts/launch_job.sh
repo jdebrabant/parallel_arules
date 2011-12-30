df87989715df7b5595a5584770de9a9e0b5460e5
Justin <debrabant@cs.brown.edu>
2011-12-30 17:12:54 -0500

    debugging scripts

--- scripts/launch_job.sh
+++ scripts/launch_job.sh
@@ -3,6 +3,7 @@
 elastic-mapreduce \
   --create \
   --jar s3n://kdd12/parallel.jar \
+  --arg s3n://kdd12/input/ \
   --arg s3n://kdd12/output/output2/ \
   --arg s3n://kdd12/output/output1/ \
   --num-instances 2 \
