07933005b60d24f983e86226b4b80a780ebbffb0
Justin <debrabant@cs.brown.edu>
2011-12-30 17:26:16 -0500

    debugging scripts

--- scripts/launch_job.sh
+++ scripts/launch_job.sh
@@ -6,5 +6,7 @@ elastic-mapreduce \
   --arg s3n://kdd12/input/ \
   --arg s3n://kdd12/output/output2/ \
   --arg s3n://kdd12/output/output1/ \
+  --log-uri s3n://kdd12/logs/ \
   --num-instances 2 \
-  --master-instance-type 
+  --instance-type m1.xlarge \
+  --master-instance-type m1.xlarge
