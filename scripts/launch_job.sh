1802e39649d841db9e0f6d42de3cc0d020a847ea
Justin <debrabant@cs.brown.edu>
2012-01-13 13:10:48 -0500

    changed launch script

--- scripts/launch_job.sh
+++ scripts/launch_job.sh
@@ -4,19 +4,18 @@ elastic-mapreduce \
   --create \
   --name "test" \
   --jar s3n://kdd12/parallel.jar \
-  --arg .1 \                            
-  --arg .1 \                            
-  --arg .2 \                            
-  --arg .1 \                            
-  --arg 100000 \                        
-  --arg 64 \                            
-  --arg 1 \                             
-  --arg s3n://kdd12/input/ \            
-  --arg s3n://kdd12/output/output1/ \   
-  --arg s3n://kdd12/output/output2/ \   
+  --arg .1 \
+  --arg .1 \
+  --arg .2 \
+  --arg .1 \
+  --arg 100000 \
+  --arg 64 \
+  --arg 1 \
+  --arg s3n://kdd12/input/ \
+  --arg s3n://kdd12/output/output1/ \
+  --arg s3n://kdd12/output/output2/ \
   --log-uri s3n://kdd12/logs/ \
   --num-instances 2 \
   --instance-type m1.xlarge \
   --master-instance-type m1.xlarge \
   --bootstrap-action s3://elasticmapreduce/bootstrap-actions/configurations/latest/memory-intensive 
-
