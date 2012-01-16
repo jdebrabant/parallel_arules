1fffc61406c9a304c9c1031586e7e5e4a4bdb2cd
Matteo Riondato <matteo_riondato@brown.edu>
2012-01-16 17:51:56 +0100

    Add Txt2SeqConverter, which converts from plain text to SequenceFile. The key used in the SequenceFile is the line number (starting from 1).

---
+++ src/util/Txt2SeqConverter.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+
+public class Txt2SeqConverter
+{
+	public static void main(String[] args)
+	{
+	  	FileSystem fs = null;
+	  	String seqFileName = args[1];
+		Configuration conf = new Configuration();
+		try {
+		  	fs = FileSystem.get(URI.create(seqFileName), conf);
+		} catch (IOException e)
+		{
+			System.out.println("ERROR: " + e.getMessage()); 
+		}
+		  
+		Path path = new Path(seqFileName);
+
+		IntWritable key = new IntWritable();
+		Text value = new Text();
+		SequenceFile.Writer writer = null;
+		try
+		{
+		  	writer = SequenceFile.createWriter(fs, conf, path, IntWritable.class, Text.class);
+			BufferedReader br = new BufferedReader(new FileReader(args[0]));
+
+			int transactionID = 1;
+			String transaction = null;
+			while ((transaction = br.readLine()) != null)
+			{
+			  key.set(transactionID);
+			  value.set(transaction);
+			  writer.append(key, value);
+
+			  transactionID++;
+			}
+		} catch (IOException e) {
+			System.out.println("ERROR: " + e.getMessage()); 
+		} finally {
+		  	IOUtils.closeStream(writer);
+		}
+	}
+}
+
