b4fbe0d7ec26ab9ce794ad1cc32cb58688b35054
Matteo Riondato <matteo_riondato@brown.edu>
2012-02-03 16:05:02 -0500

    Modify Amazon EC2 mapreduce script to include the added argument for the local confidence parameter phi. (Mock values)

--- scripts/launch_job.sh
+++ scripts/launch_job.sh
@@ -4,13 +4,14 @@ elastic-mapreduce \
   --create \
   --name "test" \
   --jar s3n://kdd12/parallel.jar \
-  --arg .1 \
-  --arg .1 \
+  --arg .02 \
+  --arg .02 \
   --arg 20 \
   --arg 10 \
   --arg 100000 \
   --arg 64 \
-  --arg 1 \
+  --arg 0.1 \
+  --arg 4 \
   --arg s3n://kdd12/input/ \
   --arg s3n://kdd12/output/output1/ \
   --arg s3n://kdd12/output/output2/ \
