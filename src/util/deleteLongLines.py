c02d3e1b17549f1047fc374dafd9b9613c1e35fd
Matteo Riondato <matteo_riondato@brown.edu>
2012-02-22 15:55:02 -0500

    Add a Python script to delete long lines. We will use it for the Netflix data.

---
+++ src/util/deleteLongLines.py
@@ -0,0 +1,27 @@
+import os, sys
+
+def errorExit(msg):
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+def main():
+    if len(sys.argv) != 3: 
+        errorExit("Usage: {} MAXLEN FILE\n".format(os.path.basename(sys.argv[0])))
+    maxlen = int(sys.argv[1])
+    fileName = sys.argv[2]
+    if not os.path.isfile(fileName):
+        errorExit("{} does not exist, or is not a file\n".format(fileName))
+    deletedLines = 0
+    with (open(fileName, 'rt')) as FILE:
+            for line in FILE:
+                items = line.split()
+                if len(items) <= maxlen:
+                    sys.stdout.write(line)
+                else:
+                    deletedLines += 1
+    sys.stderr.write("{}: {} lines deleted.\n".format(sys.argv[0], deletedLines))
+    return 0
+
+if __name__ == "__main__":
+    main()
+
